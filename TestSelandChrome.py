01e14a142cdfc5ad0e647a98337ec59e979c8bc0
ramgitprivate <156168501+ramgitprivate@users.noreply.github.com>
2024-01-30 12:34:44 +0800

    Update TestSelandChrome.py

--- TestSelandChrome.py
+++ TestSelandChrome.py
@@ -1,9 +1,11 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.firefox.options import Options
 
-def test_example_domain():
+@pytest.fixture
+def browser():
     # Firefox options (similar to Chrome options)
     firefox_options = Options()
     firefox_options.add_argument("--headless")  # Runs Firefox in headless mode.
@@ -14,6 +16,10 @@ def test_example_domain():
     # Initialize the Firefox WebDriver with options
     s = Service(geckodriver_path)
     driver = webdriver.Firefox(service=s, options=firefox_options)
+    driver.implicitly_wait(10)  # waits for the element to be present, up to 10 seconds
+    yield driver
+    driver.quit()
+
 def test_interaction_with_neo_norwexapac_org(browser):
     # Navigate to the website
     browser.get("https://NEO.norwexapac.org")
@@ -21,9 +27,9 @@ def test_interaction_with_neo_norwexapac_org(browser):
     # Perform actions, e.g., inputting text into a search field
     # Assumed there's an input field with id 'input-field'
     input_field = browser.find_element(By.ID, 'ext-comp-1004')
-    input_field.send_keys('testadmin') #ID
+    input_field.send_keys('testadmin')  # username
     input_field = browser.find_element(By.ID, 'ext-comp-1005')
-    input_field.send_keys('password') #password
+    input_field.send_keys('password')  # password
 
     # Clicking a button
     # Assumed there's a button to click with id 'submit-button'
